3d0cc9f64f71de9aa5fda445c56daa3bed3884b3
Jeremy Kun <j2kun@users.noreply.github.com>
2019-04-20 17:06:59 -0700

    add display-parsed-tree

---
+++ display-parsed-tree.py
@@ -0,0 +1,24 @@
+import sys
+from lark import Lark
+
+lark = Lark(r'''
+         tex: content+
+
+         ?content: mathmode | text+
+
+         mathmode: OFFSETDOLLAR text+ OFFSETDOLLAR
+         | OFFSETOPEN text+ OFFSETCLOSE
+         | INLINE text+ INLINE
+
+         INLINE: "$"
+         OFFSETDOLLAR: "$$"
+         OFFSETOPEN: "\\["
+         OFFSETCLOSE: "\\]"
+
+         ?text: /./s
+         ''', start='tex', parser='lalr')
+
+
+input_text = sys.stdin.read()
+parsed_doc = lark.parse(input_text)
+print(parsed_doc)
